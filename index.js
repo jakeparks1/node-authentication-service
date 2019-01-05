7207acb9e54663b6e0b087545f805984afd03273
Jake Parks <jakeparks5000@gmail.com>
2019-01-05 12:49:43 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//import express from 'express';
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:auth/auth', {useNewUrlParser: true});
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
